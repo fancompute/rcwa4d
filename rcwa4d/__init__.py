6620c172990f7f5a0d7f61c7c9b969c08f622c5f
Beicheng Lou <beicheng@Beichengs-MacBook-Air.local>
2024-09-29 21:02:34 -0500

    init

--- rcwa4d/__init__.py
+++ rcwa4d/__init__.py
@@ -1,6 +1,2 @@
 from .utils import *
 from .expansion4d import *
-
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
